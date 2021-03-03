f7adb637344a3e3cf6d8276d635e34c956e4460e
kepet19 <kevinmeejach@gmail.com>
2021-03-03 13:54:52 +0100

    add Logger as a static varible

--- src/main/java/dk/s4_g1/opc_ua_client/RunClient.java
+++ src/main/java/dk/s4_g1/opc_ua_client/RunClient.java
@@ -4,6 +4,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RunClient {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public static void main(String[] args) {
         LOGGER.log(Level.INFO, "Server started.");
     }
