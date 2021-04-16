d2424ac4dd9d988ebd22bb10383437478e1d70ce
Jakob Rasmussen <jakob1303@hotmail.com>
2021-04-16 14:25:26 +0200

    Changed Logger syntax

--- Logger/src/main/java/dk/s4_g1/logger/Logger.java
+++ Logger/src/main/java/dk/s4_g1/logger/Logger.java
@@ -5,21 +5,21 @@ import dk.s4_g1.common.services.ILog;
 public class Logger implements ILog{
     @Override
     public void Debug(String className, String msg) {
-        System.err.println("Debug<" + className + "> " + msg);
+        System.err.println("[DEBUG] <" + className + "> " + msg);
     }
 
     @Override
     public void Error(String className, String msg) {
-        System.err.println("Debug<" + className + "> " + msg);
+        System.err.println("[ERROR] <" + className + "> " + msg);
     }
 
     @Override
     public void Warning(String className, String msg) {
-        System.err.println("Debug<" + className + "> " + msg);
+        System.err.println("[WARNING]  <" + className + "> " + msg);
     }
 
     @Override
     public void Info(String className, String msg) {
-        System.err.println("Debug<" + className + "> " + msg);
+        System.err.println("[INFO]  <" + className + "> " + msg);
     }
 }
