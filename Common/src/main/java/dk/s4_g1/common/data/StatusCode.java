346287264d38926152edddbc947eb1b0ae0c8d59
kepet19 <kevinmeejach@gmail.com>
2021-04-30 13:23:45 +0200

    Update IAPIService to include some functions

---
+++ Common/src/main/java/dk/s4_g1/common/data/StatusCode.java
@@ -0,0 +1,9 @@
+package dk.s4_g1.common.data;
+
+public class StatusCode {
+    public final int statusCode;
+
+    public StatusCode(int statusCode){
+        this.statusCode = statusCode;
+    }
+}
--- Common/src/main/java/dk/s4_g1/common/services/IAPIService.java
+++ Common/src/main/java/dk/s4_g1/common/services/IAPIService.java
@@ -1,5 +1,9 @@
 package dk.s4_g1.common.services;
 
+import dk.s4_g1.common.data.StatusCode;
+
 public interface IAPIService{
-    
+    public StatusCode post(String endpoint, String data);
+    public StatusCode put(String endpoint, String data);
+    public StatusCode get(String endpoint, String data);
 }
