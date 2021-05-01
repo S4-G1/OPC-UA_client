d60818deb27580c5acdae4847e8a24f577f9d04e
kepet19 <kevinmeejach@gmail.com>
2021-05-01 12:49:09 +0200

    Removed logger, because the project will move the standart logger

--- Common/src/main/java/dk/s4_g1/common/services/ILog.java
+++
@@ -1,8 +0,0 @@
-package dk.s4_g1.common.services;
-
-public interface ILog {
-    void Debug(String className, String msg);
-    void Error(String className, String msg);
-    void Warning(String className, String msg);
-    void Info(String className, String msg);
-}
