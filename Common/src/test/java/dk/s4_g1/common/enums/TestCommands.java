a1e68ee344fd1fa789aa170dce7ed7927463cb58
kepet19 <kevinmeejach@gmail.com>
2021-05-05 16:46:44 +0200

    Add the first test for Recipes

---
+++ Common/src/test/java/dk/s4_g1/common/enums/TestCommands.java
@@ -0,0 +1,16 @@
+package dk.s4_g1.common.enums;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestCommands {
+
+    @Test
+    public void testEnumsHasTheRightID(){
+        assertEquals(1, Commands.RESET.id);
+        assertEquals(2, Commands.START.id);
+        assertEquals(3, Commands.STOP.id);
+        assertEquals(4, Commands.ABORT.id);
+        assertEquals(5, Commands.CLEAR.id);
+    }
+}
